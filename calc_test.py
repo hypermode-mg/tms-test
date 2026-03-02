cc82be1c47e485125977073b91b61f429e7a5318
Hyper Mode <hypermode@mail.ru>
2026-03-02 23:02:23 +0500

    Update calc_test.py

--- calc_test.py
+++ calc_test.py
@@ -6,10 +6,10 @@ def test_add():
     assert add(2, 3) == 5
 
 
-def test_divide():
-    assert divide(10, 2) == 5
+def test_div():
+    assert div(10, 2) == 5
 
 
-def test_divide_by_zero():
+def test_div_by_zero():
     with pytest.raises(ValueError):
-        divide(10, 0)
+        div(10, 0)
