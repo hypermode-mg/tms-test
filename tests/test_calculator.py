e42868d861ef767834889c39a040af3683824c9c
Hyper Mode <hypermode@mail.ru>
2026-03-03 00:16:40 +0500

    Change files 3

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,14 @@
 import pytest
 from app.calculator import add, div
 
+
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_div():
     assert div(10, 2) == 5
+
 
 def test_div_by_zero():
     with pytest.raises(ValueError):
